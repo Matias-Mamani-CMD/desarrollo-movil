52589ee5b9820198f2f6c906c1bb5b2c4505d2c2
Tomas Saravia <tomassaravia51@gmail.com>
2025-09-25 00:42:16 -0300

    Parte corregida

---
+++ desarrollo-mobile/src/config/firebaseConfig.js
@@ -0,0 +1,19 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAvNFg0PY28a2-bG-IYVWrIs18VVEZdFMM",
+  authDomain: "mobilestart-416a9.firebaseapp.com",
+  projectId: "mobilestart-416a9",
+  storageBucket: "mobilestart-416a9.appspot.com",
+  messagingSenderId: "268632717803",
+  appId: "1:268632717803:web:1bd3b29cfb5667401501ae",
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
